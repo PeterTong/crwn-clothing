73dc4fed8537ca7f1665adb572106d6eade5ca36
PeterTong <tkwpeter21@gmail.com>
2019-11-07 00:27:35 +0800

    Complete the form input component and custom button component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -32,12 +36,12 @@ class SignIn extends React.Component {
 
 				<form onSubmit={this.handleSubmit}>
 					{/* required is a native html attribute */}
-					<input name='email' type='email' value={this.state.email} onChange={this.handleChange} required/>
-					<label>Email</label>
-					<input name='password' type='password' value={this.state.password} onChange={this.handleChange} required/>
-					<label>Password</label>
+					<FormInput name='email' type='email' value={this.state.email} handleChange={this.handleChange} label='email' required/>
+					
+					<FormInput name='password' type='password' value={this.state.password} handleChange={this.handleChange} label='password' required/>
+					
 
-					<input type='submit' value='Submit Form'/>
+					<CustomButton type='submit' > Sign in</CustomButton>
 				</form>
 			</div>
 		)
