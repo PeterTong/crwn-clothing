f2384979aa9ac7158e47bea787db3fb8d033075e
PeterTong <tkwpeter21@gmail.com>
2019-12-18 00:51:43 +0800

    fix library issue,Complete the Checkout Page Part 1&2

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { createStructuredSelector } from 'reselect';
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurrentUser } from './redux/user/user.selectors';
@@ -54,6 +56,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage}></Route>
           <Route path="/shop" component={ShopPage}></Route>
+					<Route exact path="/checkout" component={CheckoutPage}></Route>
           <Route exact path="/signin" render={() => this.props.currentUser ? (<Redirect to='/'/>) : (<SignInAndSignUpPage/>)}></Route>
         </Switch>
       </div>
@@ -72,7 +75,10 @@ const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
 // DispatchToProps vs MapStateToProps
-// to clarify  DispatchToProps triggers a function from actions file and MapStateToProps allows you to access the state types on your reducer files correct? :)
+// to clarify  DispatchToProps triggers a function from actions file and MapStateToProps allows you to access the state types on your reducer files correct?
 
