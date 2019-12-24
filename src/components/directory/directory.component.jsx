5fb2cdf59189d623142c832c01b230f83a890ad4
PeterTong <tkwpeter21@gmail.com>
2019-12-25 01:01:07 +0800

    Complete the Collection Overview Component using it in shop component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -10,6 +10,7 @@ import "./directory.styles.scss";
 const Directory = ( { sections } ) => (
   <div className="directory-menu">
     {sections.map(({ id, ...otherSectionProps }) => (
+			
       <MenuItem key={id} {...otherSectionProps} />
     ))}
   </div>
