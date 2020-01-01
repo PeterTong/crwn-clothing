4c7a341c25c880f0a7c0f80dc9d3e299e4a1c761
PeterTong <tkwpeter21@gmail.com>
2020-01-02 00:24:54 +0800

    Complete Collection Routing and Selector, also Data Normalization with the Collection Page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { Route } from 'react-router-dom';
+
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-const ShopPage = () => (
+// Because the ShopPage is routed in App.js, so it will pass the props into it including 'match', 'history', 'location'
+// we can access 'match' props 
+const ShopPage = ({ match }) => {
+	console.log(match);
+	return (
   <div className="shop-page">
-    <CollectionsOverview />
+		<Route exact path={`${match.path}`} component={CollectionsOverview}> </Route>
+		{/* this allow us to access this categoryId as a parameter on the match object when we are inside of our category */}
+
+		<Route path={`${match.path}/:collectionId`} component={CollectionPage}></Route>
   </div>
-);
+)};
 
 export default ShopPage;
